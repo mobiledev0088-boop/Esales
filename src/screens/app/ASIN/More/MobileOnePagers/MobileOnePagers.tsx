795f81bd923bda5b94453fe55601778d89502485
Ashish Devasi <ashish_devasi@asus.com>
2026-03-06 15:25:47 +0530

    One Pager API added and Image download is working

--- src/screens/app/ASIN/More/MobileOnePagers/MobileOnePagers.tsx
+++ src/screens/app/ASIN/More/MobileOnePagers/MobileOnePagers.tsx
@@ -3,24 +3,21 @@ import {useQuery} from '@tanstack/react-query';
 import AppDropdown from '../../../../../components/customs/AppDropdown';
 import {handleASINApiCall} from '../../../../../utils/handleApiCall';
 import AppText from '../../../../../components/customs/AppText';
-import {useEffect, useMemo, useState} from 'react';
-import AppImage from '../../../../../components/customs/AppImage';
+import {useMemo, useState} from 'react';
+
 import Card from '../../../../../components/Card';
 import AppButton from '../../../../../components/customs/AppButton';
 import {screenHeight, screenWidth} from '../../../../../utils/constant';
 import {useLoaderStore} from '../../../../../stores/useLoaderStore';
-import {
-  Platform,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import {TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import AppIcon from '../../../../../components/customs/AppIcon';
 import AppModal from '../../../../../components/customs/AppModal';
 
-import {downloadFile} from '../../../../../utils/services';
-import {useLoginStore} from '../../../../../stores/useLoginStore';
 import {useUserStore} from '../../../../../stores/useUserStore';
+import {formatUnique, showToast} from '../../../../../utils/commonFunctions';
+import AppImage from '../../../../../components/customs/AppImage';
+import { downloadFile } from '../../../../../utils/services';
+import { useThemeStore } from '../../../../../stores/useThemeStore';
 
 interface MobileOnePagerModel {
   label: string;
@@ -28,46 +25,26 @@ interface MobileOnePagerModel {
 }
 
 const useGetMobileOnePagerModels = () => {
-
   const {
     Year_Qtr,
     EMP_Code: employeeCode,
     RoleId,
   } = useUserStore(state => state.empInfo);
-
   return useQuery({
     queryKey: ['mobileonepagers'],
     queryFn: async (): Promise<any> => {
-      const res = await handleASINApiCall('OnePager/Post_OnePager_getList', {
-        // username: employeeCode,
-        // Role_ID: RoleId,
-        // YearQtr: Year_Qtr,
-            username:"KN24000016",
-     Role_ID:2,
-     YearQtr:"20254"
+      const res = await handleASINApiCall('/OnePager/Post_OnePager_getList', {
+        username: employeeCode,
+        Role_ID: RoleId,
+        YearQtr: Year_Qtr,
       });
       const result = res?.DashboardData;
       console.log('Mobile One Pager List Response:', result);
-    //   if (!result?.Status)
-    //     throw new Error('Failed to fetch Mobile One Pager list');
-
-    //   const mobileOnePagerList = result?.Datainfo?.Mobile_OnePager_List || [];
-
-    //   // Deduplicate by Model_Name
-    //   return Array.from(
-    //     new Map(
-    //       mobileOnePagerList.map(
-    //         (item: {Model_Name: string; FilePath: string}) => [
-    //           item.Model_Name,
-    //           {
-    //             label: item.Model_Name.trim(),
-    //             value: item.Model_Name.trim(),
-    //             path: item.FilePath.trim(),
-    //           },
-    //         ],
-    //       ),
-    //     ).values(),
-    //   ) as any;
+      if (!result?.Status) throw new Error('Failed to fetch Mobile One Pager list');
+      return result?.Datainfo?.OnePager_List || [];
+    },
+    select: data => {
+      return formatUnique(data, 'Image_Link', 'Series','Series');
     },
   });
 };
@@ -105,14 +82,14 @@ const ImageModal = ({
             <AppIcon type="feather" name="x" size={24} color="#fff" />
           </View>
         </TouchableWithoutFeedback>
-        {/* {selectedModel && (
+        {selectedModel && (
           <AppImage
-            source={{uri: selectedModel.path}}
+            source={{uri: selectedModel.value}}
             style={{width: screenWidth * 0.95, height: screenHeight * 0.6}}
             resizeMode="contain"
             zoomable
           />
-        )} */}
+        )}
         <View className="absolute bottom-8 self-center bg-black/60 px-4 py-3 rounded-md">
           <AppText className="text-white text-base">
             Double tap to zoom in/out
@@ -128,6 +105,8 @@ export default function MobileOnePagers() {
     useState<MobileOnePagerModel | null>(null);
   const setLoading = useLoaderStore(state => state.setLoading);
   const [isOpen, setIsOpen] = useState(false);
+  
+  const isDarkMode = useThemeStore(state => state.AppTheme === 'dark');
 
   const {
     data: mobileOnePagerModels = [],
@@ -135,35 +114,30 @@ export default function MobileOnePagers() {
     isError,
   } = useGetMobileOnePagerModels();
 
-  useEffect(() => {
-    console.log('Mobile One Pager Models:', mobileOnePagerModels);
-  }, [mobileOnePagerModels]);
-
   const dropdownPlaceholder = useMemo(() => {
     if (isLoading) return 'Loading...';
     if (isError) return 'Error fetching data';
-    return 'Select Mobile One Pager';
+    return 'Select Mobile One Pager Series';
   }, [isLoading, isError]);
 
   const handleDownload = async () => {
     if (!selectedModel) return;
     try {
       setLoading(true);
+        const fileName = selectedModel.value.split('/').pop();
 
-    //   const fileName = selectedModel.path.split('/').pop();
-
-    //   await downloadFile({
-    //     url: selectedModel.path,
-    //     fileName: fileName || 'Mobile_OnePager.jpg',
-    //     autoOpen: true,
-    //   });
-      // if (result.statusCode === 200) {
-      //   // showToast('Mobile One Pager saved to Esales folder in Downloads.');
-      // } else {
-      //   console.warn('Failed to download image');
-      // }
+        const result = await downloadFile({
+          url: selectedModel.value,
+          fileName: fileName || 'Mobile_OnePager.jpg',
+          autoOpen: true,
+        });
+      if (result) {
+        showToast('Mobile One Pager saved to Esales folder in Downloads.');
+      } else {
+        showToast('Failed to download image');
+      }
     } catch (error) {
-      console.error('Error downloading image:', error);
+      showToast(`Error downloading image: ${error}`);
     } finally {
       setLoading(false);
     }
@@ -179,20 +153,21 @@ export default function MobileOnePagers() {
         placeholder={dropdownPlaceholder}
         disabled={isLoading}
         zIndex={100}
+        selectedValue={selectedModel?.value}
       />
       {selectedModel ? (
         <View>
-          <Card className="mt-5">
-            {/* <AppImage
-              source={{uri: selectedModel.path}}
+          <Card className="mt-5 border border-slate-200 dark:border-slate-700" noshadow>
+            <AppImage
+              source={{uri: selectedModel.value}}
               style={{width: screenWidth * 0.85, height: 300}}
               resizeMode="contain"
-            /> */}
+            /> 
             <View className="flex-row mt-2">
               <TouchableOpacity
                 className="p-1 bg-black/10 rounded"
                 onPress={() => setIsOpen(true)}>
-                <AppIcon type="feather" name="zoom-in" size={24} color="#000" />
+                <AppIcon type="feather" name="zoom-in" size={24} color={isDarkMode ? "#fff" : "#000"} />
               </TouchableOpacity>
             </View>
           </Card>
@@ -215,4 +190,4 @@ export default function MobileOnePagers() {
       />
     </AppLayout>
   );
-}
+};
